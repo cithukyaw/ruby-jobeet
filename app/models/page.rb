ff8ecfa5b7e3b2366e26358f4917ee52c6f06ff7
Sithu <cithukyaw@gmail.com>
2015-09-26 19:10:07 +0630

    Add Page model validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,6 @@
 class Page < ActiveRecord::Base
+    attr_accessible :title, :slug, :content
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :slug, presence: true, format: { with: /\A[a-zA-Z-]+\z/, message: "Only letters allowed" }
+    validates :content, presence: true, length: { minimum: 100 }
 end
