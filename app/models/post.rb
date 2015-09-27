447690aa1c8ee3fd2520ed02e67e9858ac6bcf67
Sithu <cithukyaw@gmail.com>
2015-09-27 13:24:55 +0630

    Add validations for the model `Post`

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 20 }
+  validates :slug, presence: true, format: { with: /\A[a-zA-Z-]+\z/, message: "Only letters allowed" }
+  validates :content, presence: true, length: { minimum: 100 }
+  validates :tag, presence: true
   belongs_to :tag
 end
