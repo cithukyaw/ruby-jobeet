ec327986f3f664d7965670cad9fe29a48eceefc7
Sithu <cithukyaw@gmail.com>
2015-10-03 21:16:49 +0630

    Update minimum length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 20 }
+  validates :title, presence: true, length: { minimum: 10 }
   validates :slug, presence: true, format: { with: /\A[a-zA-Z-]+\z/, message: "Only letters allowed" }
-  validates :content, presence: true, length: { minimum: 100 }
+  validates :content, presence: true, length: { minimum: 50 }
   validates :tag, presence: true
   belongs_to :tag
 end
