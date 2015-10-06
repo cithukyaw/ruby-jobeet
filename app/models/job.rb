6d6208df103cead86f1b5642237b9b407ff1bfb7
Sithu <sithu@lr-i.biz>
2015-10-06 16:17:05 +0630

    Add active record callbacks for Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -10,6 +10,8 @@ class Job < ActiveRecord::Base
   validates :how_to_apply, presence: true, length: { minimum: 50 }
   validates :email, presence: true, email: true
 
+  before_create :set_expires_at_value, :set_token_value
+
   def employment_types
     [
       ['Full Time', 'full-time'],
@@ -18,4 +20,18 @@ class Job < ActiveRecord::Base
       ['Internship', 'internship']
     ]
   end
+
+  protected
+    def set_expires_at_value
+      if self.expires_at.nil?
+        self.expires_at = Time.new + (30 * 24 * 60 * 60)
+      end
+    end
+
+    def set_token_value
+      if self.token.nil?
+        value = self.email + rand(11111..99999).to_s
+        self.token = Digest::SHA1.hexdigest value
+      end
+    end
 end
