b1c5e6c8161a3eadd639ead96b629529cdafa06f
Sithu <cithukyaw@gmail.com>
2015-10-08 22:54:38 +0630

    Generate ActiveAdmin using `rails g active_admin:install`

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
