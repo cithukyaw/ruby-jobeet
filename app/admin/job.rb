71871cc0bde46476b7cf512f7d68f43307f0d125
Sithu <cithukyaw@gmail.com>
2015-10-13 21:49:15 +0630

    Customize admin Job form

--- app/admin/job.rb
+++ app/admin/job.rb
@@ -26,4 +26,24 @@ ActiveAdmin.register Job do
     actions
   end
 
+  form do |f|
+    f.inputs do
+      f.input :category
+      f.input :employment_type, as: :select, collection: Job::EMPLOYMENT_TYPES
+      f.input :position
+      f.input :company
+      f.input :url
+      f.input :location
+      f.input :description
+      f.input :how_to_apply
+      f.input :email
+      f.input :expires_at,
+        as: :datepicker,
+        datepicker_options: { date_format: "yy-mm-dd", min_date: Time.to_s + "+1D" },
+        input_html: { :value => f.object.expires_at ? f.object.expires_at.strftime('%Y-%m-%d') : (Date.today+7).to_s } # default +7 days
+      f.input :is_public
+      f.input :is_activated
+    end
+    actions
+  end
 end
