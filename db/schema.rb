98935b5a516d3005c7af9c7fff7671e6b53d7ee5
Sithu <cithukyaw@gmail.com>
2015-10-25 15:16:56 +0630

    Schema updated for friendly_id and slug in Job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008161830) do
+ActiveRecord::Schema.define(version: 20151025065314) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -53,6 +53,19 @@ ActiveRecord::Schema.define(version: 20151008161830) do
     t.datetime "updated_at"
   end
 
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
   create_table "jobs", force: true do |t|
     t.string   "employment_type", limit: 20,                  null: false
     t.string   "company",         limit: 50,                  null: false
@@ -70,6 +83,7 @@ ActiveRecord::Schema.define(version: 20151008161830) do
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   add_index "jobs", ["category_id"], name: "index_jobs_on_category_id", using: :btree
