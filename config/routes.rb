18506a0258a730a9a5d932816729de258c9c4a09
Sithu <cithukyaw@gmail.com>
2015-11-21 22:41:56 +0630

    Change home page routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'pages' => 'pages#index'
 
-  root 'pages#index'
+  root 'jobs#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
