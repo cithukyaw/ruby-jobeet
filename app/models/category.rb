9dcdc2416af100cfecb47f4167879f7de6b82415
Sithu <cithukyaw@gmail.com>
2015-11-28 18:20:50 +0630

    Remove the custom property Category.active_jobs and use the association Category.jobs

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,14 +2,6 @@ class Category < ActiveRecord::Base
   has_many :jobs
   default_scope { order(created_at: :desc) }
 
-  def active_jobs=(value)
-    @active_jobs = value
-  end
-
-  def active_jobs
-    @active_jobs
-  end
-
   def no_of_jobs=(value)
     @no_of_jobs = value
   end
